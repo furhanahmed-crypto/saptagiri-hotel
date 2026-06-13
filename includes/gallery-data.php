d2b905aaf39c2047570e05ea6d6dd04fcb82e3f2
Farhan-6710 <itz.me.farhan6710@gmail.com>
2026-06-13 13:53:58 +0530

    Refactor gallery data by removing outdated images and adding new images for deluxe and suite rooms. Clean up the rooms and banquet sections to ensure the gallery reflects the latest offerings.

--- includes/gallery-data.php
+++ includes/gallery-data.php
@@ -9,8 +9,6 @@ return [
     ['path' => 'assets/images/hotel-common/about-us-day.png', 'category' => 'hotel', 'caption' => 'Hotel Exterior (Day)'],
     ['path' => 'assets/images/hotel-common/about-us.png', 'category' => 'hotel', 'caption' => 'Hotel Exterior (Night)'],
     ['path' => 'assets/images/hotel-common/Hotel-in-secunderabad..jpg', 'category' => 'hotel', 'caption' => 'Hotel in Secunderabad'],
-    ['path' => 'assets/images/hotel-common/Hotel-in-hyderabad-vegetarian-hotel.png', 'category' => 'hotel', 'caption' => 'Vegetarian Hotel'],
-    ['path' => 'assets/images/hotel/DSC03798.jpg', 'category' => 'hotel', 'caption' => 'Hotel Exterior'],
     ['path' => 'assets/images/hotel/DSC03532-2.jpg', 'category' => 'hotel', 'caption' => 'Hotel Facade'],
     ['path' => 'assets/images/hotel/DSC03875.jpg', 'category' => 'hotel', 'caption' => 'Hotel Building'],
     ['path' => 'assets/images/hotel/DSC04174.jpg', 'category' => 'hotel', 'caption' => 'Hotel Ambience'],
@@ -33,25 +31,20 @@ return [
     ['path' => 'assets/images/hotel-common/Hotel-near-secunderabad-railway-station.png', 'category' => 'hotel', 'caption' => 'Near Secunderabad Railway Station'],
 
     // Rooms
-    ['path' => 'assets/images/suite-room/hotel-saptagiri-suite-room-overview-img-1.jpg', 'category' => 'rooms', 'caption' => 'Suite Room'],
-    ['path' => 'assets/images/suite-room/premium-suite-room-hotel-secunderabad-img-1.jpg', 'category' => 'rooms', 'caption' => 'Premium Suite Room'],
-    ['path' => 'assets/images/suite-room/hotel-saptagiri-suite-room-img-1.jpg', 'category' => 'rooms', 'caption' => 'Suite Living Area'],
-    ['path' => 'assets/images/suite-room/premium-suite-room-hotel-secunderabad-img-2.jpg', 'category' => 'rooms', 'caption' => 'Suite Bedroom'],
-    ['path' => 'assets/images/suite-room/hotel-saptagiri-suite-room-img-3.jpg', 'category' => 'rooms', 'caption' => 'Suite Interior'],
-    ['path' => 'assets/images/deluxe-room/premium-deluxe-room-hotel-saptagiri-paradise-junction-img-3.jpg', 'category' => 'rooms', 'caption' => 'Premium Deluxe Room'],
-    ['path' => 'assets/images/deluxe-room/deluxe-room-secunderabad-img-2.jpg', 'category' => 'rooms', 'caption' => 'Deluxe Room'],
     ['path' => 'assets/images/deluxe-room/hotel-saptagiri-deluxe-room-secunderabad-railway-station-img-1.jpg', 'category' => 'rooms', 'caption' => 'Deluxe Room'],
     ['path' => 'assets/images/hotel/Delux-Room1.jpg', 'category' => 'rooms', 'caption' => 'Deluxe Room'],
     ['path' => 'assets/images/hotel/dx-room1.jpg', 'category' => 'rooms', 'caption' => 'Deluxe Room'],
     ['path' => 'assets/images/deluxe-room/hotel-saptagiri-deluxe-room-img-4.jpg', 'category' => 'rooms', 'caption' => 'Deluxe Room'],
+    ['path' => 'assets/images/suite-room/hotel-saptagiri-suite-room-overview-img-1.jpg', 'category' => 'rooms', 'caption' => 'Suite Room'],
+    ['path' => 'assets/images/suite-room/premium-suite-room-hotel-secunderabad-img-1.jpg', 'category' => 'rooms', 'caption' => 'Premium Suite Room'],
+    ['path' => 'assets/images/suite-room/hotel-saptagiri-suite-room-img-1.jpg', 'category' => 'rooms', 'caption' => 'Suite Living Area'],
+    ['path' => 'assets/images/suite-room/premium-suite-room-hotel-secunderabad-img-2.jpg', 'category' => 'rooms', 'caption' => 'Suite Bedroom'],
+    ['path' => 'assets/images/deluxe-room/premium-deluxe-room-hotel-saptagiri-paradise-junction-img-3.jpg', 'category' => 'rooms', 'caption' => 'Premium Deluxe Room'],
     ['path' => 'assets/images/deluxe-room/deluxe-room-secunderabad-img-5.jpg', 'category' => 'rooms', 'caption' => 'Deluxe Room'],
     ['path' => 'assets/images/deluxe-room/premium-deluxe-room-hotel-saptagiri-paradise-junction-img-4.jpg', 'category' => 'rooms', 'caption' => 'Deluxe Room'],
-    ['path' => 'assets/images/standard-room/hotel-saptagiri-standard-room-secunderabad-railway-station-img-1.jpg', 'category' => 'rooms', 'caption' => 'Standard Room'],
     ['path' => 'assets/images/standard-room/hotel-saptagiri-standard-room-passport-office-img-1.jpg', 'category' => 'rooms', 'caption' => 'Standard Room'],
     ['path' => 'assets/images/standard-room/budget-standard-room-secunderabad-img-3.jpg', 'category' => 'rooms', 'caption' => 'Budget Standard Room'],
     ['path' => 'assets/images/standard-room/budget-business-standard-room-secunderabad-img-2.jpg', 'category' => 'rooms', 'caption' => 'Business Standard Room'],
-    ['path' => 'assets/images/standard-room/hotel-saptagiri-standard-room-img-2.jpg', 'category' => 'rooms', 'caption' => 'Standard Room'],
-    ['path' => 'assets/images/standard-room/hotel-saptagiri-standard-room-twin-beds-img-3.jpg', 'category' => 'rooms', 'caption' => 'Standard Room (Twin Beds)'],
 
     // Dining
     ['path' => 'assets/images/restaurant/Hotel-saptagiri-pure-vegetarian-restaurant.jpg', 'category' => 'dining', 'caption' => 'Pure Vegetarian Restaurant'],
@@ -68,7 +61,6 @@ return [
     ['path' => 'assets/images/banquet-hall/Hotel-saptagiri-banquet-hall.jpg', 'category' => 'banquets', 'caption' => 'Banquet Hall'],
     ['path' => 'assets/images/banquet-hall/Banquet-hall-secunderabad.jpg', 'category' => 'banquets', 'caption' => 'Banquet Hall Secunderabad'],
     ['path' => 'assets/images/banquet-hall/Pure-Veg-banquet-venue-hotel-saptagiri.jpg', 'category' => 'banquets', 'caption' => 'Pure Veg Banquet Venue'],
-    ['path' => 'assets/images/banquet-hall/birthday-party-hall-secunderabad.jpg', 'category' => 'banquets', 'caption' => 'Birthday Party Hall'],
     ['path' => 'assets/images/banquet-hall/corporate-event-hall-hyderabad.jpg', 'category' => 'banquets', 'caption' => 'Corporate Event Hall'],
 
     // Board Room & Conference Halls
@@ -76,9 +68,5 @@ return [
     ['path' => 'assets/images/board-room/Conference-room-secunderabad.jpg', 'category' => 'boardrooms', 'caption' => 'Conference Room'],
     ['path' => 'assets/images/board-room/Conference-room-hyderabad.jpg', 'category' => 'boardrooms', 'caption' => 'Conference Room Hyderabad'],
     ['path' => 'assets/images/board-room/Corporate-board-room-Secunderabad.jpg', 'category' => 'boardrooms', 'caption' => 'Corporate Board Room'],
-    ['path' => 'assets/images/board-room/Corporate-board-room-hyderabad.jpg', 'category' => 'boardrooms', 'caption' => 'Corporate Board Room Hyderabad'],
-    ['path' => 'assets/images/board-room/Business-meeting-room-hotel-saptagiri.jpg', 'category' => 'boardrooms', 'caption' => 'Business Meeting Room'],
-    ['path' => 'assets/images/board-room/business-meeting-room-hyderabad.jpg', 'category' => 'boardrooms', 'caption' => 'Business Meeting Room'],
-    ['path' => 'assets/images/hotel/boardroom1.jpg', 'category' => 'boardrooms', 'caption' => 'Board Room'],
     ['path' => 'assets/images/hotel/boardroom5.jpg', 'category' => 'boardrooms', 'caption' => 'Board Room'],
 ];
